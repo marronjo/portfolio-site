e3ec656be1e88c3387b47d52f2131c40419de752
Joe Marron <68810618+marronjo@users.noreply.github.com>
2021-09-14 12:32:17 +0100

    public

--- client/src/ui/Card.js
+++ client/src/ui/Card.js
@@ -5,8 +5,8 @@ function Card(){
     return(
         <div>
             <footer>
-                <a alt="Github" href="https://www.github.com/marronjo"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/></a>
-                <a alt="LinkedIn" href="https://www.linkedin.com/in/marronjo"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"/></a>
+                <a href="https://www.github.com/marronjo"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github"/></a>
+                <a href="https://www.linkedin.com/in/marronjo"><img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="LinkedIn"/></a>
             </footer>
         </div>
     );
